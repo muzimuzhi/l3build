1f4a738ce32cff962d237b8290575df5f6e20903
Yukai Chou <muzimuzhi@gmail.com>
2023-02-21 00:13:51 +0800

    Enable pdf-based tests for dvips

--- config-pdf.lua
+++ config-pdf.lua
@@ -1,2 +1,4 @@
-checkengines = {"pdftex", "xetex"}
+checkengines = {"pdftex", "xetex", "latexdvips"}
+-- 2 runs are needed by xetex, to get the right <prism:pageCount> value
+checkruns = 2
 testfiledir  = "testfiles-pdf"
