4bce6633b153ce87e24a173c875b56fb2ff87d51
Yukai Chou <muzimuzhi@gmail.com>
2023-02-22 02:43:27 +0800

    wip: add ps2pdf opt `-dNOSAFER`

--- config-pdf.lua
+++ config-pdf.lua
@@ -2,3 +2,4 @@ checkengines = {"pdftex", "xetex", "latexdvips"}
 -- 2 runs are needed by xetex, to get the right <prism:pageCount> value
 checkruns = 2
 testfiledir  = "testfiles-pdf"
+ps2pdfopt = " -dNOSAFER "
